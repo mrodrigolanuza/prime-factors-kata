1e8c3a4886a037ad28f430ceb2d17f3bfa497380
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-21 19:39:02 +0200

    Red: finds the prime sequence of a particular number

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,5 +1,20 @@
-describe("The Prime Factorizer should..", ()=>{
-    it("Prueba",()=>{
-        expect(true).toBe(false);
+/*
+- 2 ⇒ [2]
+- 2 * 2 ⇒ [2,2]
+- 2 * 2 * 2 ⇒ [2,2,2]
+- 3 ⇒ [3]
+- 3 * 3 ⇒ [3,3]
+- 3 * 2 ⇒ [2,3]
+- 5 * 5 ⇒ [5,5]
+- 5 * 7 * 11 * 3 ⇒ [3,5,7,11]
+*/
+
+describe("The Prime Factorizer", ()=>{
+    it("finds the prime sequence of a particular number",()=>{
+        expect(getPrimeFactorOfNumber(2)).toEqual([2]);
     });
 });
+
+function getPrimeFactorOfNumber(number:number){
+    return null;
+}
