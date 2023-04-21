90062455dee7af16a990f21e59a3bdd32413c802
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-21 21:03:54 +0200

    Refactor: extract methods

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,16 +1,20 @@
 export function getPrimeFactorOfNumber(number:number){
+    checkIsPositiveNumber(number);
+    return primeFactors(number);
+}
+
+function checkIsPositiveNumber(number){
+    if(number <= 0) throw Error("Must be a number greater than zero");
+}
+
+function primeFactors(number:number){
     const prime = findSmallestPrime(number);
     const remainder = number / prime;
     return remainder <= 1 ? [prime] : [prime].concat(getPrimeFactorOfNumber(remainder));
 }
 
 function findSmallestPrime(number:number){
-    if(number <= 0)
-        throw Error("Must be a number greater than zero");
-    
-    if(number === 1)
-        return 1;
-    
+    if(number === 1) return 1;
     let factor = 2;
     while(number % factor != 0){
         factor++;
